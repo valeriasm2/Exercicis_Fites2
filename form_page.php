851d657713a327f93b015430d1b714fdb75cfc8f
valeria <vsantanamegia01.cf@iesesteveterradas.cat>
2025-09-26 17:06:07 +0200

    formularios

---
+++ form_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulario de ejemplo</title>
+</head>
+<body>
+    <form method="post" action="/entorn_page.php">
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname"><br>
+
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname"><br>
+
+        <input type="submit" value="Enviar">
+    </form> 
+</body>
+</html>
